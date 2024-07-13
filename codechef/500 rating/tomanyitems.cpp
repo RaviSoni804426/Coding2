6490693f78c1032cd9113e50cc0af5eab9d7e7df
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-07-13 11:21:04 +0530

    codedchef code

---
+++ codechef/500 rating/tomanyitems.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+
+int main()
+{
+    ll t;
+    cin >> t ;
+    while(t--)
+    {
+        int n;
+        cin >> n ;
+        cout << (n+9)/10 << '\n' ;
+    }
+
+    return 0;
+}
