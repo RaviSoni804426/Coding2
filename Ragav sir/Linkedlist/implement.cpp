95a6379e17f43b758e802dc5dad72b90065de95f
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-07-23 22:15:55 +0530

    code

---
+++ Ragav sir/Linkedlist/implement.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    public:
+    node(int data1,node* next1){
+        data=data1;
+        next=next1;
+
+    }
