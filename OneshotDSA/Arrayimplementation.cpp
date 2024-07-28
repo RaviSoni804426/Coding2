156c5d997df5904e3529b5bc34b426d1f037c00c
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-07-28 17:40:13 +0530

    code

--- OneshotDSA/Arrayimplementation.cpp
+++ OneshotDSA/Arrayimplementation.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 int main(){
     int size=5;
-    int arr[size];
-     
+    int arr[size]={1,2,3,4,8};
+    
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+       int count=0;  
+        for(int j=0;j<i;j++){
+            if(arr[j]%2!=0) count++;
+        }
+    }
+    vector<int>result;
+    int k=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]%2!=0){
+            result[k]=arr[i];
+            k++;
+        }
+    }
+    cout<<result[k];
+    
+}
+
+    
---
+++ OneshotDSA/linkedlist.cpp
@@ -0,0 +1,4 @@
+#include<bits/stdc++.h>
+using namespace std;
+   int n=arr.length;
+   int dp[n][n];`
