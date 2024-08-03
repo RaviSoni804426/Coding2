3c473e3cf3c74ee82f1388767ae949d4d1120ff2
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-08-03 19:06:54 +0530

    code

--- OneshotDSA/linkedlist.cpp
+++ OneshotDSA/linkedlist.cpp
@@ -1,36 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-   struct Node{
-    int data;
-    Node* next;
-    Node(int value){
-        data=value;
-        next=NULL;
-    }
-   };
-   class Linkedlist{
-    Node* head;
-    Linkedlist(){
-        head=NULL;
-    }
-    void insertAtbeginning(int value){
-        Node* newNode=new Node(value);
-        newNode->next=head;
-        head=newNode;
-    }
-    void insertatend(int value){
-        Node* newNode=new Node(value);
-        if(head==NULL){
-            head=newNode;
-            return;
-        }
-        Node* temp=head;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=newNode;
-    }
-    void deleteNode
+// struct Node{
+//     int value;
+//     Node* next;
+//     Node(int value){
+//         this->value = value;
+//         this->next = NULL;
+//     }
+// };
+// int main(){
+//     Node a(10);
+//     Node b(20);
+//    Node c(30);
+//     a.next = &b;
+//     b.next = &c;
+//     c.next=NULL;
+//     Node temp=a;
+//     while(1){
+//         cout<<temp.value<<" ";
+//         if(temp.next==NULL) break;
+//         temp=*(temp.next);
+//     }
     
 
-   }
+// }
+
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+    }
+};
+int main(){
+    Node* a=new Node(4);
+    Node* b=new Node(3);
+    Node* c=new Node(2);
+    a->next=b;
+    b->next=c;
+    cout<<a->value<<" ";
+    
+}
+
