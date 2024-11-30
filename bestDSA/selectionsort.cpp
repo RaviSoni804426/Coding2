e993524fe1877406a33402b6af12db1eb2b65f5c
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-11-30 22:17:34 +0530

    code

--- bestDSA/selectionsort.cpp
+++ bestDSA/selectionsort.cpp
@@ -1,7 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+for (int i=1;i<n;i++){
+    int j=1;
+    while(j>=1 && arr[j]<arr[j-1]){
+        swap(arr[j], arr[j-1]);
+        j--;
+    }
+}
 
 
-    
 }
