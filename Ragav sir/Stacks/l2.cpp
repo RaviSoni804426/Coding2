40662a2ffa623d134c6e836a0dab6354146cf111
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-12-10 22:07:22 +0530

    code

---
+++ Ragav sir/Stacks/l2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display
+int main(){
+    stack<int>st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    display(st);
+    
+}
