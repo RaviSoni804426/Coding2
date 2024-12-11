bf6e5a568e77b42416c99e7af6642949438ef93d
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-12-11 12:00:29 +0530

    code

---
+++ Ragav sir/Stacks/validpaaren.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string s)
+    {
+     stack<char>st;
+     st.push(s[0]);
+     for(int i=1;i<s.length();i++){
+         if(s[i]!=st.top()) st.push(s[i]);
+     }
+     s=" ";
+     while(st.size()>0){
+         s+=st.top();
+         st.pop();
+     }
+     reverse(s.begin(),s.end());
+     return s;
+    }
+};
