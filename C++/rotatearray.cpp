d4a1fb27a51407b091ba61f54a1575a34dfe793d
RaviSoni804426 <rk8715514@gamil.com>
2025-02-22 09:11:16 +0530

    code

---
+++ C++/rotatearray.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+int main(){
+      void rotate(vector<int> &arr) {
+          int n=arr.size();
+          for( int i=n-1;i>0;i--){
+              cout<<arr[i];
+          }
+          return arr;
+      }
+  }
